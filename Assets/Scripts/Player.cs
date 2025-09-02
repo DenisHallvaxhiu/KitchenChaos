b5ec62793c0b95dd1c5f8192fabff7c4b04c2406
DenisHallvaxhiu <82787471+DenisHallvaxhiu@users.noreply.github.com>
2025-09-02 07:12:12 -0400

    MoveDir/Visual

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,16 +1,33 @@
+using JetBrains.Annotations;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField]private float moveSpeed = 7f;
     private void Update()
     {
+        Vector2 inputVector = new Vector2(0, 0);
+
         if (Input.GetKey(KeyCode.W))
         {
-            Debug.Log("Key pressed");
+            inputVector.y = 1;
         }
-        else
+        if (Input.GetKey(KeyCode.A))
         {
-            Debug.Log("Key not pressed");
+           inputVector.x = -1;
         }
+        if (Input.GetKey(KeyCode.D))
+        {
+            inputVector.x = 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            inputVector.y = -1;
+        }
+        inputVector = inputVector.normalized;
+
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        transform.position += moveDir * Time.deltaTime * moveSpeed;
     }
 }
