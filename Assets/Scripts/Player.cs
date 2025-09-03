02bc7cd890bdeafd427f4d80b32ac7cca8ddf1c1
DenisHallvaxhiu <82787471+DenisHallvaxhiu@users.noreply.github.com>
2025-09-03 15:03:29 -0400

    New input system

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,14 +1,17 @@
 using JetBrains.Annotations;
 using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour
 {
     [SerializeField]private float moveSpeed = 7f;
+    [SerializeField] private GameScript gameScript;
 
     private bool isWalking;
     private void Update()
     {
-        
+        Vector2 inputVector = gameScript.GetMovementVectorNormalized();
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         isWalking = moveDir != Vector3.zero;
