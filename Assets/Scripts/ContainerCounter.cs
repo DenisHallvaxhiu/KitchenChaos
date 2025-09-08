1183e0d60126286ab3b6252a0f42c887764c9f09
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2025-09-08 08:00:43 -0400

    BasePrefab Counter

---
+++ Assets/Scripts/ContainerCounter.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class ContainerCounter : MonoBehaviour, IKitchenObjectParent {
+    [SerializeField] private KitchenObjectsSO kitchenObjectsSO;
+    [SerializeField] private Transform counterTopPoint;
+
+    private KitchenObject kitchenObject;
+
+
+
+    public void Interact(Player player) {
+        if(kitchenObject == null) {
+            Transform kitchenObjectTransform = Instantiate(kitchenObjectsSO.prefab,counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetKitchenObjectParent(this);
+        }
+        else {
+            //Give object to player
+            //Debug.Log(kitchenObject.GetKitchenObjectParent());
+            kitchenObject.SetKitchenObjectParent(player);
+        }
+
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+    public void SetKitchenObject(KitchenObject kitchenObject) {
+        this.kitchenObject = kitchenObject;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
+    }
+}
