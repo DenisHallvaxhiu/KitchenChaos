a50287b5409d016b8e35ba2b5bbf1ca7d20c8efc
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2025-09-20 09:19:15 -0400

    Plate UI Icons

---
+++ Assets/Scripts/PlateIconSingleUI.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+using UnityEngine.UI;
+public class PlateIconSingleUI : MonoBehaviour {
+    [SerializeField] private Image image;
+    public void SetKitchenObjectSO(KitchenObjectsSO kitchenObjectsSO) {
+        image.sprite = kitchenObjectsSO.sprite;
+    }
+}
--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -35,4 +35,8 @@ public class PlateKitchenObject : KitchenObject {
         }
 
     }
+
+    public List<KitchenObjectsSO> GetKitchenObjectsSOList() {
+        return kitchenObjectSOList;
+    }
 }
