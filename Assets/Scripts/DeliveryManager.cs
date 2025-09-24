a63ff1ab97ee0f42d4eef85db075eef87047b89b
DenisHallvaxhiu <dhallvaxhiu999@gmail.com>
2025-09-24 08:23:14 -0400

    Recipe UI Display

--- Assets/Scripts/DeliveryManager.cs
+++ Assets/Scripts/DeliveryManager.cs
@@ -1,7 +1,11 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class DeliveryManager : MonoBehaviour {
+
+    public event EventHandler OnRecipeSpawned;
+    public event EventHandler OnRecipeCompleted;
 
     public static DeliveryManager Instance {
         get; private set;
@@ -24,10 +28,9 @@ public class DeliveryManager : MonoBehaviour {
             spawnRecipeTimer = spawnRecipeTimerMax;
 
             if(waitingRecipeSOList.Count < waitingRecipesMax) {
-                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[Random.Range(0,recipeListSO.recipeSOList.Count)];
-                Debug.Log(waitingRecipeSO.recipeName);
+                RecipeSO waitingRecipeSO = recipeListSO.recipeSOList[UnityEngine.Random.Range(0,recipeListSO.recipeSOList.Count)];
                 waitingRecipeSOList.Add(waitingRecipeSO);
-
+                OnRecipeSpawned?.Invoke(this,EventArgs.Empty);
             }
         }
 
@@ -59,8 +62,9 @@ public class DeliveryManager : MonoBehaviour {
                 }
                 if(plateContentsMatchesRecipe) {
                     //player delivered correct recipe
-                    Debug.Log("Player delivered correct recipe");
                     waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeCompleted?.Invoke(this,EventArgs.Empty);
                     return;
                 }
             }
@@ -68,6 +72,10 @@ public class DeliveryManager : MonoBehaviour {
         //No matches found
         //no correct recipe delivered
         Debug.Log("Incorrect item");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
     }
 
 }
